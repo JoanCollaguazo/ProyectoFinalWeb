30071fe366b59e851cb7a9773217d7c9d3de5e6d
OxOrboy <joangamers12@gmail.com>
2021-09-30 08:02:39 -0500

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  constructor(private formBuider: FormBuilder) { }
+
+  formLogin = this.formBuider.group({
+    nombres: ['', [Validators.required]],
+    password1: ['', [Validators.required, Validators.minLength(8)]],
+    password2: ['', [Validators.required, Validators.minLength(8)]]
+  });
+
+
+
+
+
+  ngOnInit() {
+    /*
+      this.formLogin = this.formBuider.group({
+        //verificar con validator que el valor ingresado sea requerido y ver si es email
+        nombres:['',[Validators.required]],
+        apellidos:['',[Validators.required]],
+        empresa:['',[Validators.required]],
+        usuario:['',[Validators.required,Validators.name]],
+        email:['',[Validators.required,Validators.email]],
+        password: ['',Validators.required,Validators.minLength(6)]
+  
+      });
+      */
+  }
+
+  submit(): any {
+    console.log(this.formLogin.value);
+  }
 }
