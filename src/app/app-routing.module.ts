f943fbf5acc44860aea48088ef16e98fe4fcd7a1
OxOrboy <joangamers12@gmail.com>
2021-09-30 09:27:53 -0500

    Inicio - Loggin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './app/inicio/inicio.component';
+import { LogginComponent } from './app/loggin/loggin.component';
 
-const routes: Routes = [];
+//AQUI VAN TODAS LAS RUTAS DE LAS PAGINAS:
+const routes: Routes = [
+  {path: '',component:InicioComponent},
+  {path: 'loggin',component:LogginComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
